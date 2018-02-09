d68ad5f42e084ec578f542cd0b58e07f1da75292
Cindey Caine <ccaine@ucsd.edu>
2018-02-08 17:18:25 -0800

    fine-tuning still

--- pyimagesearch/lenet.py
+++ pyimagesearch/lenet.py
@@ -5,6 +5,7 @@ from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
+from keras.layers.core import Dropout
 from keras import backend as K
 
 class LeNet:
@@ -19,20 +20,27 @@ class LeNet:
 			inputShape = (depth, height, width)
 
 		# first set of CONV => RELU => POOL layers
-		model.add(Conv2D(20, (5, 5), padding="same",
+		model.add(Conv2D(64, (3, 3), padding="same",
 			input_shape=inputShape))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
 		# second set of CONV => RELU => POOL layers
-		model.add(Conv2D(50, (5, 5), padding="same"))
+		model.add(Conv2D(128, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        
+		# third set of CONV => RELU => POOL layers
+		model.add(Conv2D(256, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+              
 
 		# first (and only) set of FC => RELU layers
 		model.add(Flatten())
-		model.add(Dense(500))
+		model.add(Dense(1000))
 		model.add(Activation("relu"))
+		model.add(Dropout(0.5))
 
 		# softmax classifier
 		model.add(Dense(classes))
